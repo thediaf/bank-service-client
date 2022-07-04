ad560f53e513ebaa6db8ca7989d58fa16094d03b
thediaf <jaharacs98@gmail.com>
2022-07-04 00:22:50 +0000

    feat: design & conversion form create

--- index.php
+++ index.php
@@ -1,24 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banque Web Service</title>
+</head>
+<body>
 <?php
-
-    $client = new SoapClient("http://localhost:8000/BankService?wsdl");
-
-    $response = $client->__soapCall("getAccounts", []);
-?>
-    <table border="2">
-        <tr>
-            <th>Code</th>
-            <th>Solde</th>
-            <th>Date de creation</th>
-        </tr>
-<?php
-    foreach ($response->return as $account) {
-?>
-        <tr>
-           <td> <?= $account->code ?></td>
-           <td> <?= $account->balance ?></td>
-           <td> <?= $account->createdAt ?></td>
-        </tr>
-<?php
+    if (isset($_POST['method'])) {
+        $client = new SoapClient("http://localhost:8000/BankService?wsdl");
+        if ($_POST['method'] == "conversion") 
+        {
+            # code...
+        }
+        else
+        {
+            $accounts = $client->__soapCall("getAccounts", []);
+        }
     }
+
+?>
+
+    <form action="index.php" method="post">
+        Montant : <input type="text" name="amount">
+        <input type="submit" value="conversion" name="method">
+        <input type="submit" value="comptes" name="method">
+    </form>
+    <?php
+    if (isset($accounts)) {
+        # code...
+?>    
+    <table border="2" style="margin: 7px;">
+    <tr>
+        <th>Code</th>
+        <th>Solde</th>
+        <th>Date de creation</th>
+    </tr>
+<?php
+        # code...
+foreach ($accounts->return as $account) {
+?>
+    <tr>
+       <td> <?= $account->code ?></td>
+       <td> <?= $account->balance ?></td>
+       <td> <?= $account->createdAt ?></td>
+    </tr>
+<?php
+}
 ?>
 </table>
+<?php
+}
+?>
+</body>
+</html>
