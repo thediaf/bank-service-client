a93e07dcb96e7838a704abd6c1f922bc8f90996a
thediaf <jaharacs98@gmail.com>
2022-07-04 01:06:12 +0000

    feat: finishhhhhhh :)

--- index.php
+++ index.php
@@ -7,56 +7,61 @@
     <title>Banque Web Service</title>
 </head>
 <body>
-<?php
-    if (isset($_POST['method'])) {
-        $client = new SoapClient("http://localhost:8000/BankService?wsdl");
-        if ($_POST['method'] == "conversion") 
+    <?php
+    if (isset($_POST['method'])) 
+    {
+        $client = new SoapClient("http://localhost:8020/BankService?wsdl");
+
+        if ($_POST['method'] == "xofToEuro") 
         {
-            $result = $client->__soapCall("conversion", ["amount" => $_POST['amount']]);
+            $param = new stdClass();
+            $param->amount = $_POST['amount'];
+            $result = $client->__soapCall("xofToEuro", array($param));
+        }
+        elseif ($_POST['method'] == "euroToXof") 
+        {
+            $param = new stdClass();
+            $param->amount = $_POST['amount'];
+            $result = $client->__soapCall("euroToXof", array($param));
         }
         else
-        {
             $accounts = $client->__soapCall("getAccounts", []);
-        }
     }
-
-?>
+    ?>
 
     <form action="index.php" method="post">
         Montant : <input type="text" name="amount">
-        <input type="submit" value="conversion" name="method">
+        <input type="submit" value="xofToEuro" name="method">
+        <input type="submit" value="euroToXof" name="method">
         <input type="submit" value="comptes" name="method">
     </form>
     <?php
-        if (isset($result)) {
-            
+        if (isset($result))
             echo "Resultat: " . $_POST['amount'] . " = " . $result->return;
-
-        }
-    if (isset($accounts)) {
-        # code...
-?>    
+        elseif (isset($accounts)) 
+        {
+    ?>   
+    <p>Liste des comptes:</p> 
     <table border="2" style="margin: 7px;">
-    <tr>
-        <th>Code</th>
-        <th>Solde</th>
-        <th>Date de creation</th>
-    </tr>
-<?php
-        # code...
-foreach ($accounts->return as $account) {
-?>
-    <tr>
-       <td> <?= $account->code ?></td>
-       <td> <?= $account->balance ?></td>
-       <td> <?= $account->createdAt ?></td>
-    </tr>
-<?php
-}
-?>
-</table>
-<?php
-}
-?>
+        <tr>
+            <th>Code</th>
+            <th>Solde</th>
+            <th>Date de creation</th>
+        </tr>
+    <?php
+        foreach ($accounts->return as $account) {
+    ?>
+        <tr>
+            <td> <?= $account->code ?></td>
+            <td> <?= $account->balance ?></td>
+            <td> <?= $account->createdAt ?></td>
+        </tr>
+    <?php
+        }
+    ?>
+    </table>
+    <?php
+        }
+    ?>
 </body>
 </html>
